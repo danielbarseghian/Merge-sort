e91b103f26628ad2e741e35da88bbc5d037b0c01
Daniel Barseghian <daniel@daniel-deb-laptop.daniel-deb.org>
2026-04-29 14:02:36 +0200

    changing pcs

---
+++ merge.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int ARRLEN = 8;
+int main(void)
+{
+    int arr[ARRLEN];
+
+    // Randomly generate the array
+
+    // Print the unsorted
+
+    // Sort it
+
+    // Print the sorted list
+}
